aa8be757f61e4d7c9937057362d30e1c01c1a3ce
Shaggy <mohammadali.bscs@gmail.com>
2021-10-08 15:38:15 +0500

    functional by js

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-function changeStartText(number) {
-    const stext = document.querySelector('::after');
-    stext.style.opacity = number;
+function changeBgColor(color){
+    const cont = document.querySelector('.overlay');
+    cont.style.background = color;
+}
+function imgSlider(anything){
+    document.querySelector('.bg').src=anything;
+}
+function boxSlider(color){
+    const boxSlider = document.querySelector('.box');
+    boxSlider.style.background = color;
+}
+function iconSlider(anything){
+    document.querySelector('.menu').src=anything;
 }
